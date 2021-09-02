5ec422eeec3047652f344a1ec1bb47da5de4497e
DanWuPet <89942751+DanWuPet@users.noreply.github.com>
2021-09-02 14:30:57 +0200

    Added string "Welcome User"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// Print string
         System.out.println("Hello world!");
+	System.out.println("Welcome User!");
     }
 }
